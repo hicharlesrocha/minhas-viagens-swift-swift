9468ccb5a7bf26b32ddb4b59f234d44c452dd808
Hicharles <hicharlesolvr@gmail.com>
2018-05-06 23:25:25 -0300

    Configurações iniciais

---
+++ minhas viagens/minhas viagens/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  minhas viagens
+//
+//  Created by Hicharles Rocha on 05/05/2018.
+//  Copyright © 2018 Hicharles. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+
+    @IBOutlet weak var mapa: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
